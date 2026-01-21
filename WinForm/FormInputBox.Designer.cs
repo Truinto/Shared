2268f2d5057001e013fb68e611b24a17233dd71d
Truinto <47917181+Truinto@users.noreply.github.com>
2026-01-21 15:11:53 +0100

    added FormInputBox

--- WinForm/FormInputBox.Designer.cs
+++ WinForm/FormInputBox.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Shared
 {
-    partial class FormMessageBox
+    partial class FormInputBox
     {
         /// <summary>
         /// Required designer variable.
@@ -30,23 +30,20 @@
         {
             this.Button1 = new Button();
             this.Button2 = new Button();
-            this.Button3 = new Button();
-            this.Button4 = new Button();
             this.LabelTime = new Label();
-            this.TableLayoutPanel1 = new TableLayoutPanel();
             this.Label1 = new Label();
-            this.TableLayoutPanel1.SuspendLayout();
+            this.TextBox1 = new TextBox();
             SuspendLayout();
             // 
             // Button1
             // 
             this.Button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             this.Button1.AutoSize = true;
-            this.Button1.Location = new Point(3, 3);
+            this.Button1.Location = new Point(206, 130);
             this.Button1.Name = "Button1";
             this.Button1.Size = new Size(75, 25);
-            this.Button1.TabIndex = 3;
-            this.Button1.Text = "Button1";
+            this.Button1.TabIndex = 1;
+            this.Button1.Text = "OK";
             this.Button1.UseVisualStyleBackColor = true;
             this.Button1.Click += Button1_Click;
             // 
@@ -54,38 +51,13 @@
             // 
             this.Button2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             this.Button2.AutoSize = true;
-            this.Button2.Location = new Point(84, 3);
+            this.Button2.Location = new Point(287, 130);
             this.Button2.Name = "Button2";
             this.Button2.Size = new Size(75, 25);
             this.Button2.TabIndex = 2;
-            this.Button2.Text = "Button2";
+            this.Button2.Text = "Cancel";
             this.Button2.UseVisualStyleBackColor = true;
-            this.Button2.Visible = false;
             this.Button2.Click += Button2_Click;
-            // 
-            // Button3
-            // 
-            this.Button3.AutoSize = true;
-            this.Button3.Location = new Point(165, 3);
-            this.Button3.Name = "Button3";
-            this.Button3.Size = new Size(75, 25);
-            this.Button3.TabIndex = 1;
-            this.Button3.Text = "Button3";
-            this.Button3.UseVisualStyleBackColor = true;
-            this.Button3.Visible = false;
-            this.Button3.Click += Button3_Click;
-            // 
-            // Button4
-            // 
-            this.Button4.AutoSize = true;
-            this.Button4.Location = new Point(246, 3);
-            this.Button4.Name = "Button4";
-            this.Button4.Size = new Size(75, 25);
-            this.Button4.TabIndex = 0;
-            this.Button4.Text = "Button4";
-            this.Button4.UseVisualStyleBackColor = true;
-            this.Button4.Visible = false;
-            this.Button4.Click += Button4_Click;
             // 
             // LabelTime
             // 
@@ -94,65 +66,51 @@
             this.LabelTime.Location = new Point(12, 131);
             this.LabelTime.Name = "LabelTime";
             this.LabelTime.Size = new Size(36, 23);
-            this.LabelTime.TabIndex = 5;
+            this.LabelTime.TabIndex = 3;
             this.LabelTime.Text = "0:00";
             this.LabelTime.TextAlign = ContentAlignment.MiddleCenter;
             this.LabelTime.Visible = false;
-            // 
-            // TableLayoutPanel1
-            // 
-            this.TableLayoutPanel1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            this.TableLayoutPanel1.AutoSize = true;
-            this.TableLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            this.TableLayoutPanel1.BackColor = Color.Transparent;
-            this.TableLayoutPanel1.ColumnCount = 4;
-            this.TableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
-            this.TableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
-            this.TableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
-            this.TableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
-            this.TableLayoutPanel1.Controls.Add(this.Button4, 3, 0);
-            this.TableLayoutPanel1.Controls.Add(this.Button1, 0, 0);
-            this.TableLayoutPanel1.Controls.Add(this.Button3, 2, 0);
-            this.TableLayoutPanel1.Controls.Add(this.Button2, 1, 0);
-            this.TableLayoutPanel1.GrowStyle = TableLayoutPanelGrowStyle.AddColumns;
-            this.TableLayoutPanel1.Location = new Point(44, 127);
-            this.TableLayoutPanel1.Name = "TableLayoutPanel1";
-            this.TableLayoutPanel1.RowCount = 1;
-            this.TableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            this.TableLayoutPanel1.Size = new Size(324, 31);
-            this.TableLayoutPanel1.TabIndex = 6;
             // 
             // Label1
             // 
             this.Label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             this.Label1.AutoSize = true;
-            this.Label1.Location = new Point(68, 9);
+            this.Label1.Location = new Point(65, 9);
             this.Label1.MaximumSize = new Size(297, 0);
-            this.Label1.MinimumSize = new Size(297, 100);
+            this.Label1.MinimumSize = new Size(297, 70);
             this.Label1.Name = "Label1";
-            this.Label1.Size = new Size(297, 100);
-            this.Label1.TabIndex = 7;
+            this.Label1.Size = new Size(297, 70);
+            this.Label1.TabIndex = 4;
             this.Label1.Text = "Text here";
             this.Label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // FormMessageBox
+            // TextBox1
+            // 
+            this.TextBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            this.TextBox1.BorderStyle = BorderStyle.FixedSingle;
+            this.TextBox1.Location = new Point(12, 90);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new Size(350, 23);
+            this.TextBox1.TabIndex = 0;
+            // 
+            // FormInputBox
             // 
             this.AutoScaleDimensions = new SizeF(7F, 15F);
             this.AutoScaleMode = AutoScaleMode.Font;
             this.BackColor = SystemColors.ControlLightLight;
             this.ClientSize = new Size(374, 166);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.Button2);
             this.Controls.Add(this.Label1);
-            this.Controls.Add(this.TableLayoutPanel1);
             this.Controls.Add(this.LabelTime);
+            this.Controls.Add(this.Button1);
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "FormMessageBox";
+            this.Name = "FormInputBox";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = FormStartPosition.CenterParent;
-            this.TableLayoutPanel1.ResumeLayout(false);
-            this.TableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -160,10 +118,8 @@
         #endregion
         private Button Button1;
         private Button Button2;
-        private Button Button3;
-        private Button Button4;
         private Label LabelTime;
-        private TableLayoutPanel TableLayoutPanel1;
         private Label Label1;
+        private TextBox TextBox1;
     }
 }
---
+++ WinForm/FormInputBox.cs
@@ -0,0 +1,162 @@
+﻿using Shared.CollectionNS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shared
+{
+    public partial class FormInputBox : Form
+    {
+        public TimeSpan Timeout;
+        private System.Windows.Forms.Timer? _Timer1;
+        private TimeSpan _CurrentTimeout;
+
+        public FormInputBox(string text, string caption, MessageBoxIcon icon, TimeSpan timeout = default)
+        {
+            InitializeComponent();
+
+            AcceptButton = Button1;
+            CancelButton = Button2;
+
+            Label1.Text = text;
+            base.Text = caption;
+            this.Timeout = timeout;
+
+            Icon = icon switch
+            {
+                MessageBoxIcon.Warning => SystemIcons.Warning,
+                MessageBoxIcon.Error => SystemIcons.Error,
+                MessageBoxIcon.Question => SystemIcons.Question,
+                _ => SystemIcons.Information,
+            };
+
+            RecalculateSize();
+        }
+
+        public void RecalculateSize()
+        {
+            this.Width = 390;
+            this.Label1.MinimumSize = new(this.Width - 93, 70); // stretch text to window width
+            this.Label1.MaximumSize = new(this.Width - 93, 0);
+            this.Height = Math.Max(205, this.Label1.Height + 135); // stretch window height to fix text
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            if (Timeout != default)
+            {
+                _CurrentTimeout = Timeout;
+                LabelTime.Text = Timeout.ToString("mm\\:ss");
+                LabelTime.Visible = true;
+                if (_Timer1 == null)
+                {
+                    _Timer1 = new();
+                    _Timer1.Interval = 1000;
+                    _Timer1.Tick += Timer_Tick;
+                }
+                _Timer1.Start();
+            }
+            else
+            {
+                LabelTime.Visible = false;
+            }
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            _Timer1?.Stop();
+            base.OnFormClosed(e);
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            _CurrentTimeout = _CurrentTimeout.Subtract(TimeSpan.FromSeconds(1));
+            if (_CurrentTimeout <= TimeSpan.Zero)
+                Button1.PerformClick();
+            else
+                LabelTime.Text = _CurrentTimeout.ToString("mm\\:ss");
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            if (Icon != null)
+            {
+                e.Graphics.DrawIconUnstretched(Icon, new Rectangle(20, this.Height / 2 - 75, Icon.Width, Icon.Height)); //43
+            }
+        }
+
+        private Brush _BackgroundBrush = new SolidBrush(SystemColors.Control);
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            base.OnPaintBackground(e);
+
+            e.Graphics.FillRectangle(_BackgroundBrush, 0, Button1.Top - 12, this.Width, 50);
+        }
+
+        public new string Text
+        {
+            get => Label1.Text;
+            set => Label1.Text = value;
+        }
+
+        public string Caption
+        {
+            get => base.Text;
+            set => base.Text = value;
+        }
+
+        public string ButtonText1
+        {
+            get => Button1.Text;
+            set
+            {
+                Button1.Text = value;
+                Button1.Visible = value is not (null or "");
+            }
+        }
+
+        public string ButtonText2
+        {
+            get => Button2.Text;
+            set
+            {
+                Button2.Text = value;
+                Button2.Visible = value is not (null or "");
+            }
+        }
+
+        public string InputBoxText
+        {
+            get => TextBox1.Text;
+            set => TextBox1.Text = value;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        public static DialogResult Show(string text, string caption, MessageBoxIcon icon, TimeSpan timeout = default)
+        {
+            return new FormInputBox(text, caption, icon, timeout).ShowDialog();
+        }
+    }
+}
